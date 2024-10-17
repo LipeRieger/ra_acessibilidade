7285a0917192d6c79c85ca1f91ebc4e3c914e81f
Felipe Rieger <liperieger@gmail.com>
2024-10-17 14:10:53 -0300

    sdfrg53te

---
+++ ProjetoRA/Assets/Project/MenuController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("Function2");
+    }
+
+}
